a4773941cb56d2cae6a870821e0dca1842384e7a
Matthew Jacobs <5423966+mattj256@users.noreply.github.com>
2018-03-04 01:55:27 -0500

    adding first problem solution

---
+++ src/main/java/com/dreamsmadevisible/kattis/quadrantselection/QuadrantSelection.java
@@ -0,0 +1,19 @@
+package com.dreamsmadevisible.kattis.simple.quadrantselection;
+
+import java.util.Scanner;
+
+public class QuadrantSelection {
+  public QuadrantSelection() { }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int x = scanner.nextInt();
+    int y = scanner.nextInt();
+
+    if (x > 0) {
+      System.out.println(y > 0 ? 1 : 4);
+    } else {
+      System.out.println(y > 0 ? 2 : 3);
+    }
+  }
+}
