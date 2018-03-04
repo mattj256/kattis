64b9b614ff5f108b8157040d2f3f829c08e27466
Matthew Jacobs <5423966+mattj256@users.noreply.github.com>
2018-03-04 15:33:09 -0500

    adding Carrots solution

---
+++ src/main/java/com/dreamsmadevisible/kattis/carrots/Carrots.java
@@ -0,0 +1,17 @@
+package com.dreamsmadevisible.kattis.carrots;
+
+import java.util.Scanner;
+
+/**
+ * Problem definition: https://open.kattis.com/problems/carrots
+ */
+public class Carrots {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    // Don't need to save this
+    scanner.nextInt();
+    System.out.println(scanner.nextInt());
+    // Don't need to read the rest of the input.
+  }
+}
