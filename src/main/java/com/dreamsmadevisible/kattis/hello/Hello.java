542997292e4d79e9e370f66834070135e5e5225a
Matthew Jacobs <5423966+mattj256@users.noreply.github.com>
2018-03-04 03:07:22 -0500

    adding Hello solution

---
+++ src/main/java/com/dreamsmadevisible/kattis/hello/Hello.java
@@ -0,0 +1,11 @@
+package com.dreamsmadevisible.kattis.hello;
+
+/**
+ * Problem definition: https://open.kattis.com/problems/hello
+ */
+public class Hello {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
